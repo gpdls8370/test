af6d974e1e62c4a3a12f72fa888fb09762b2cc9b
ycdoh <ycdoh@172.20.10.9>
2019-01-02 15:32:23 +0900

    test

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=0;
+		
+		for(int i=0;i<10;i++)
+		{
+			n = n + i;
+		
+				
+			System.out.println(" " + n);
+	
+		}
+
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,24 @@
+
+public class Test {
+
+	private int x;
+	private int y;
+	final int data = 1;
+	
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public interface myInterface{
+		public void method1();
+		public void method2(int x, int y);
+		
+		public int method3(int x, int y);
+		
+		
+	}
+	
+	
+	
+}
